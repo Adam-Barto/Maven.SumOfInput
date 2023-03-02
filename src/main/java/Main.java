345a74c4ce50fcea7392759fec1a03913570c23b
Adam B <adamb@Zipcoders-MacBook-Pro.local>
2023-03-02 16:24:36 -0500

    Completed the Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,56 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
         
         int number = getInputNumber();
+        long t1 = System.currentTimeMillis();
         
         int sum = sumOfNumbers(number);
         
+        long t2 = System.currentTimeMillis();
+        long lengthOfTime = t2-t1;
+        
         System.out.println(sum);
+
+        System.out.println("Wizard time: ");
+        t1 = System.currentTimeMillis();
+
+        sum = gaussianSumOfNumbers(number);
+
+        t2 = System.currentTimeMillis();
+
+        System.out.println(sum);
+
+        long gaussylength = t2-t1;
+
+        if (lengthOfTime < gaussylength)
+            System.out.println("The Wizard has been defeated!");
+        else if (lengthOfTime > gaussylength)
+            System.out.println("Gaussian Method has won! The Wizard has Triumphed");
+        else
+            System.out.println("They are the same!");
 
     }
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {//added static
+        int counter = 1;
+        int add = 0;
+        while (counter != (n+1)){
+            add = add + counter;
+            ++counter;
+        }
+        return add;
     }
     
     
-    int getInputNumber() {
+    static int getInputNumber() {//added Static
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
@@ -35,8 +65,10 @@ public class Main {
     
     
     // for Extra Credit
-    int gaussianSumOfNumbers(int n) {
-        return 0;
+    static int gaussianSumOfNumbers(int n) { //This is much faster but for ints, not very helpful
+        int add = n*(n+1);
+        add = add/2;
+        return add;
     }
    
     // for Extra extra credit - compare the methods and show which one is faster
